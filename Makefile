fae7c232d31ac187322bfec4c83e6aa83db1e7f2
ivan <ivan@LatitudeE6520.informatik.uni-leipzig.de>
2017-02-23 16:10:26 +0100

    need to test on infai servers, not enough RAM on my local pc

---
+++ Makefile
@@ -0,0 +1,3 @@
+clean:
+	rm -rf /data/hadoop/namenode/*
+	rm -rf /data/hadoop/datanode/*
---
+++ edit-hosts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "127.0.0.1 hdfs-namenode.demo.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 hdfs-resourcemanager.demo.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 hdfs-historyserver.demo.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 hdfs-nodemanager.demo.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 hdfs-datanode.demo.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 hue.demo.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 spark-master.demo.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 spark-worker.demo.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 demo.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 monitor.demo.big-data-europe.local" >> /etc/hosts
+echo "127.0.0.1 virtuoso.demo.big-data-europe.local" >> /etc/hosts
+
